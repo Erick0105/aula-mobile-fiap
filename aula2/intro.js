ce4fa39f894b21174207a10f1ff4642840f2c862
Erick0105 <erickaxs0105@gmail.com>
2025-02-13 10:36:20 -0300

    ternary operator

--- aula2/intro.js
+++ aula2/intro.js
@@ -1,1 +1,17 @@
-// condicao ? valor se verdadeiro : valor se falso
+var x = 3 
+
+// Operador Ternario
+//  condicao ? valor se verdadeiro : valor se falso
+
+const resultado = x%2==0 ? "Número é par" : "Número é impar"
+console.log(resultado)
+
+
+// num>0 Positivo
+// num<0 Negativo
+// num==0 Zero
+
+var num = 15
+
+const estado = num==0?"Zero": num>0 ? "Positivo":"Negativo"
+console.log(estado)
