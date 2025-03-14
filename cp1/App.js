3a24b3e2c15b80e7254f097ba0d4d189baf3b28a
Erick0105 <erickaxs0105@gmail.com>
2025-03-14 14:20:53 -0300

    estilização n é legal em mobile

--- cp1/App.js
+++ cp1/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Text, View, Image, TextInput, Button } from 'react-native';
+import { Text, View, Image, TextInput, Button , StyleSheet} from 'react-native';
 import Calculate from './components/calculate.js';  
 
 export default function App() {
@@ -13,29 +13,32 @@ export default function App() {
   const handlePercentChange = (text) => setPercentProduct(text);
 
   return (
-    <View>
-      <Image source={require('./assets/products.png')} />
-      <View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('./assets/products.png')} />
+      <View style={styles.information}>
         <Text>CP1 Mobile Application Developer</Text>
         <Text>Erick Alves | RM 556862</Text>
       </View>
+      
 
-      <View>
+      <View >
         
         <View>
-          <Text>Insira o nome do produto</Text>
+          <Text style={styles.text}>Insira o nome do produto</Text>
           <TextInput 
+            style={styles.inputs}
             placeholder='Nome do produto' 
             maxLength={100} 
             value={nameProduct} 
             onChangeText={handleNameChange} 
           />
         </View>
-
+        
         
         <View>
-          <Text>Qual o valor atual desse produto?</Text>
+          <Text style={styles.text}>Qual o valor atual desse produto?</Text>
           <TextInput 
+            style={styles.inputs}
             placeholder='Preço' 
             keyboardType='decimal-pad' 
             maxLength={100} 
@@ -46,8 +49,9 @@ export default function App() {
 
         
         <View>
-          <Text>Em % qual será o aumento no preço deste produto?</Text>
-          <TextInput 
+          <Text style={styles.text}>Em % qual será o aumento no preço deste produto?</Text>
+          <TextInput
+            style={styles.inputs}
             placeholder='Percentual' 
             keyboardType='number-pad' 
             maxLength={100} 
@@ -72,3 +76,32 @@ export default function App() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: "#00B4E5",
+    flex:1,
+    alignItems:'center',
+    justifyContent:'space-evenly'
+  },
+  image:{
+    resizeMode:'center',
+    height:100,
+  },
+  information:{
+    borderColor:'#153B50',
+    borderBottomWidth:5,
+    height:50,
+    justifyContent:'space-around',
+  },
+  inputs:{
+    backgroundColor:'#ECEBE4',
+    marginVertical:20,
+    borderRadius:10,
+  },
+  text:{
+    fontSize:16,
+    fontWeight:'bold',
+    color:'#565254'
+  }
+});
